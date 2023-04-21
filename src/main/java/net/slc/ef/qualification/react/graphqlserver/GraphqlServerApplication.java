bf59d2608d91e9b0a8f3633d7ffccfb8600cb0e3
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-04-22 00:19:48 +0700

    Pre-populate data

--- src/main/java/net/slc/ef/qualification/react/graphqlserver/GraphqlServerApplication.java
+++ src/main/java/net/slc/ef/qualification/react/graphqlserver/GraphqlServerApplication.java
@@ -1,5 +1,7 @@
 package net.slc.ef.qualification.react.graphqlserver;
 
+import net.slc.ef.qualification.react.graphqlserver.model.Player;
+import net.slc.ef.qualification.react.graphqlserver.model.PlayerStatistics;
 import net.slc.ef.qualification.react.graphqlserver.service.PlayerService;
 import net.slc.ef.qualification.react.graphqlserver.service.PlayerStatisticsService;
 import org.springframework.boot.ApplicationRunner;
@@ -17,8 +19,70 @@ public class GraphqlServerApplication {
     @Bean
     protected ApplicationRunner init(PlayerService playerService, PlayerStatisticsService playerStatisticsService) {
         return args -> {
+            Player player = new Player();
+            player.setName("BadAccuracyID");
+            Long id = playerService.createPlayer(player).getId();
 
+            PlayerStatistics playerStatistics = playerStatisticsService.getPlayerStatisticsByPlayerId(id).orElseThrow();
+            playerStatistics.setWins(100);
+            playerStatistics.setLosses(18);
+            playerStatistics.setKills(1000);
+            playerStatistics.setDeaths(200);
+            playerStatistics.setAssists(500);
+            playerStatistics.setMoney(100000);
+            playerStatisticsService.updatePlayerStatistics(playerStatistics);
 
+            player = new Player();
+            player.setName("sh_likes_to_win");
+            id = playerService.createPlayer(player).getId();
+
+            playerStatistics = playerStatisticsService.getPlayerStatisticsByPlayerId(id).orElseThrow();
+            playerStatistics.setWins(1000);
+            playerStatistics.setLosses(1);
+            playerStatistics.setKills(10000);
+            playerStatistics.setDeaths(2000);
+            playerStatistics.setAssists(5000);
+            playerStatistics.setMoney(1000000);
+            playerStatisticsService.updatePlayerStatistics(playerStatistics);
+
+            player = new Player();
+            player.setName("FlxC4t");
+            id = playerService.createPlayer(player).getId();
+
+            playerStatistics = playerStatisticsService.getPlayerStatisticsByPlayerId(id).orElseThrow();
+            playerStatistics.setWins(84);
+            playerStatistics.setLosses(16);
+            playerStatistics.setKills(5437);
+            playerStatistics.setDeaths(1234);
+            playerStatistics.setAssists(2345);
+            playerStatistics.setMoney(1000);
+            playerStatisticsService.updatePlayerStatistics(playerStatistics);
+
+            player = new Player();
+            player.setName("sh_likes_to_lose");
+            id = playerService.createPlayer(player).getId();
+
+            playerStatistics = playerStatisticsService.getPlayerStatisticsByPlayerId(id).orElseThrow();
+            playerStatistics.setWins(1);
+            playerStatistics.setLosses(1000);
+            playerStatistics.setKills(2000);
+            playerStatistics.setDeaths(10000);
+            playerStatistics.setAssists(5000);
+            playerStatistics.setMoney(1000000);
+            playerStatisticsService.updatePlayerStatistics(playerStatistics);
+
+            player = new Player();
+            player.setName("elseif_condition");
+            id = playerService.createPlayer(player).getId();
+
+            playerStatistics = playerStatisticsService.getPlayerStatisticsByPlayerId(id).orElseThrow();
+            playerStatistics.setWins(100);
+            playerStatistics.setLosses(100);
+            playerStatistics.setKills(1000);
+            playerStatistics.setDeaths(1000);
+            playerStatistics.setAssists(1000);
+            playerStatistics.setMoney(100000);
+            playerStatisticsService.updatePlayerStatistics(playerStatistics);
         };
     }
 }

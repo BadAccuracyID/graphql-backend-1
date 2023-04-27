91b8f67c9a6e393d1d6e5c32d9f63cb3d68b65f6
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-04-28 01:57:36 +0700

    Fixed cors

--- src/main/java/net/slc/ef/qualification/react/graphqlserver/AppConfig.java
+++ src/main/java/net/slc/ef/qualification/react/graphqlserver/AppConfig.java
@@ -12,16 +12,24 @@ public class AppConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("*"); // Allow requests from any origin
+        configuration.addAllowedOrigin("http://localhost:3000"); // Allow requests from any origin
         configuration.addAllowedMethod("POST"); // Allow only POST requests
-        configuration.addAllowedMethod("GET"); // Allow only POST requests
+        configuration.addAllowedMethod("GET"); // Allow only GET requests
+        configuration.addAllowedMethod("DELETE"); // Allow only GET requests
+        configuration.addAllowedMethod("HEAD"); // Allow only GET requests
         configuration.addAllowedHeader("authorization"); // Allow the authorization header
+        configuration.addAllowedHeader("content-type"); // Allow the content type header
+        configuration.addAllowedHeader("Access-Control-Allow-Origin"); // Allow the Access-Control-Allow-Origin header
+        configuration.addAllowedHeader("Access-Control-Allow-Headers"); // Allow the Access-Control-Allow-Headers header
+        configuration.addAllowedHeader("Access-Control-Allow-Methods"); // Allow the Access-Control-Allow-Methods header
+        configuration.addAllowedHeader("Access-Control-Allow-Credentials"); // Allow the Access-Control-Allow-Credentials header
+        configuration.addAllowedHeader("Access-Control-Max-Age"); // Allow the Access-Control-Max-Age header
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // Apply CORS to the endpoint
-        source.registerCorsConfiguration("/graphql/**", configuration); // Apply CORS to the GraphQL endpoint
-
+        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/graphql/**", configuration);
         return source;
     }
+
 
 }

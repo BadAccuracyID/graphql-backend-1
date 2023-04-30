6d981fa6fa51e344f8dbdd252a9635fcab7dc03a
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2023-05-01 01:11:37 +0700

    Fixed cors and endpoint mapping

--- src/main/java/net/slc/ef/qualification/react/graphqlserver/AppConfig.java
+++ src/main/java/net/slc/ef/qualification/react/graphqlserver/AppConfig.java
@@ -1,35 +1,17 @@
 package net.slc.ef.qualification.react.graphqlserver;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer {
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:3000"); // Allow requests from any origin
-        configuration.addAllowedMethod("POST"); // Allow only POST requests
-        configuration.addAllowedMethod("GET"); // Allow only GET requests
-        configuration.addAllowedMethod("DELETE"); // Allow only GET requests
-        configuration.addAllowedMethod("HEAD"); // Allow only GET requests
-        configuration.addAllowedHeader("authorization"); // Allow the authorization header
-        configuration.addAllowedHeader("content-type"); // Allow the content type header
-        configuration.addAllowedHeader("Access-Control-Allow-Origin"); // Allow the Access-Control-Allow-Origin header
-        configuration.addAllowedHeader("Access-Control-Allow-Headers"); // Allow the Access-Control-Allow-Headers header
-        configuration.addAllowedHeader("Access-Control-Allow-Methods"); // Allow the Access-Control-Allow-Methods header
-        configuration.addAllowedHeader("Access-Control-Allow-Credentials"); // Allow the Access-Control-Allow-Credentials header
-        configuration.addAllowedHeader("Access-Control-Max-Age"); // Allow the Access-Control-Max-Age header
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        source.registerCorsConfiguration("/graphql/**", configuration);
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*");
     }
-
 
 }
--- src/main/java/net/slc/ef/qualification/react/graphqlserver/controller/playerstats/PlayerStatisticsMutator.java
+++ src/main/java/net/slc/ef/qualification/react/graphqlserver/controller/playerstats/PlayerStatisticsMutator.java
@@ -5,10 +5,10 @@ import net.slc.ef.qualification.react.graphqlserver.service.PlayerStatsService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@CrossOrigin
+@RequestMapping("/graphql")
 public class PlayerStatisticsMutator {
 
     private final PlayerStatsService playerStatsService;
